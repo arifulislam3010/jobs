b303b19c20a0d62e9f46387a8ec5378113e488be
moinul4960 <moinulhasan.4960@gmail.com>
2021-06-26 19:13:42 +0600

    homepage data formation done

--- pages/index.vue
+++ pages/index.vue
@@ -1,108 +1,81 @@
 <template>
-    <div>
-      <div class="row">
-        <div class="col-md-12">
-          <Slider></Slider>
-          <breackingNews ></breackingNews>
-        </div>
-        <div class="col-md-12">
-          <Content1></Content1>
-        </div>
-      </div>
-      <Content2></Content2>
-      <div class="col-md-12">
-        <div class="container">
-          <div class="row">
-            <div class="col-12 on-mobile-col-12">
-              <Content3></Content3>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-        <!-- <form v-if="!$store.state.authUser" @submit.prevent="login">
-      <p v-if="formError" class="error">
-        {{ formError }}
-      </p>
-      <p><i>To login, use <b>demo</b> as username and <b>demo</b> as password.</i></p>
-      <p>Username: <input v-model="formUsername" type="text" name="username"></p>
-      <p>Password: <input v-model="formPassword" type="password" name="password"></p>
-      <button type="submit">
-        Login
-      </button>
-    </form> -->
-        <!-- <hero>
-            <div class="mt-4 mb-5">
-                <logo></logo>
-                <p class="lead text-white">Hello World</p>
-            </div>
-        </hero> -->
-</template>
-<script>
+  <div class="main-body">
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis esse
+      quo dolor ea maiores eum, voluptates, obcaecati consequuntur sunt, nam
+      unde molestiae! Unde laborum facere nostrum eius eos accusantium quis.
+    </p>
+    <!-- <test-nav> </test-nav> -->
+    <!-- hero section -->
+    <HeroSection />
+    <!-- job categories -->
+    <JobCategory :category="this.$store.state.jobs.all_jobs_category" />
+    <div class="mb-5"></div>
+    <!-- hot jobs -->
+    <HotJobs />
+    <div class="mb-5"></div>
+    <!-- career section -->
+    <CarrerSection />
+    <div class="mb-2"></div>
+    <!-- Features -->
+    <Features />
+    <!-- success story -->
+    <SuccessStory />
+    <div class="mb-5"></div>
+    <!-- support -->
+    <Support />
+    <div class="mb-5"></div>
 
-import { isMobile } from 'mobile-device-detect';
-import Hero from "~/components/argon-demo/Hero";
-import Slider from "~/components/custom/slider/index.vue";
-import breackingNews from "~/components/custom/breackingNews.vue";
-import Content1 from "~/components/custom/home/content1.vue";
-import Content2 from "~/components/custom/home/content2.vue";
-import Content3 from "~/components/custom/home/content3.vue";
-import Logo from "~/components/Logo.vue";
+    <!-- vendor -->
+    <Vendor />
+    <div class="mb-4"></div>
+    <!-- footer -->
+    <Footer />
+  </div>
+</template>
+
+<script>
+// import TestNav from '~/components/customDesign/test/test.vue'
+import Footer from '~/components/customDesign/Footer/Footer.vue'
+import CarrerSection from '~/components/customDesign/Career/Career.vue'
+import HotJobs from '~/components/customDesign/HotJobs/HotJobs.vue'
+import SuccessStory from '~/components/customDesign/SuccessSotry/SuccessStory.vue'
+import Features from '~/components/customDesign/Features/Features.vue'
+import JobCategory from '~/components/customDesign/JobCategory/JobCategory.vue'
+
+import Support from '~/components/customDesign/Support/Support.vue'
+import HeroSection from '~/components/customDesign/HeroSection/HeroSection.vue'
+import Vendor from '~/components/customDesign/Vendor/Vendor.vue'
+
 export default {
-  
+  layout: 'customDesign',
+
   components: {
-      Hero,
-      Content1,
-      Content2,
-      Content3,
-      Logo,
-      Slider,
-      breackingNews
+    // TestNav,
+    Footer,
+    CarrerSection,
+    HotJobs,
+    SuccessStory,
+    Features,
+
+    JobCategory,
+    Support,
+    HeroSection,
+    Vendor
   },
-  data () {
+  data() {
     return {
-      formError: null,
-      formUsername: '',
-      formPassword: '',
-      isMobile: isMobile,
-      
+      category: []
     }
   },
   methods: {
-    async login () {
-      try {
-        await this.$store.dispatch('login', {
-          username: this.formUsername,
-          password: this.formPassword
-        })
-        this.formUsername = ''
-        this.formPassword = ''
-        this.formError = null
-        alert('ss');
-
-      } catch (e) {
-        this.formError = e.message
-      }
-
-         
-
-    },
-    async logout () {
-      try {
-        await this.$store.dispatch('logout')
-      } catch (e) {
-        this.formError = e.message
-      }
-    }
+    // this.$store.dispatch()
   },
-  mounted(){
-   
+  created() {
+    this.category = this.$store.dispatch('jobs/job_category')
+    console.log(this.$store.state.jobs.all_jobs_category)
   }
-};
-</script>
-<style>
-@media (max-width: 425px){
-  .on-mobile-col-12 { padding: 0; }
 }
-</style>
+</script>
 
+<style lang="scss"></style>
