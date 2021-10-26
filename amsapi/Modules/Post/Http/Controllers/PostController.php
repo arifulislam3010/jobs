ba717918c71ba30d839eff5bc4d0cf8ae2b413a7
moinul4960 <moinulhasan.4960@gmail.com>
2021-10-27 01:55:00 +0600

    job salary

--- amsapi/Modules/Post/Http/Controllers/PostController.php
+++ amsapi/Modules/Post/Http/Controllers/PostController.php
@@ -16,20 +16,20 @@ use App\Jobs\SendReminderEmail;
 use App\User;
 
 
-
 use Modules\Post\Entities\PostCategory;
 use Modules\Post\Transformers\Post as PostResource;
 use Modules\Post\Transformers\PostCategory as PostCategoryResource;
 
-use Modules\Post\Transformers\PostDetail ;
+use Modules\Post\Transformers\PostDetail;
 
 use Modules\Setting\Entities\Ncategory;
 
 use Modules\Setting\Transformers\Ncategory as NcategoryResource;
 
-use Modules\Post\Transformers\ReporterPost ;
+use Modules\Post\Transformers\ReporterPost;
 use Carbon\Carbon;
 use Auth;
+
 class PostController extends Controller
 {
     /**
@@ -39,25 +39,22 @@ class PostController extends Controller
     public function index($type)
     {
         $auth_user_id = Auth()->user()->id;
-        if($type == 1){
-            if(Auth()->user()->type == 1 || Auth()->user()->type == 9){
-                return PostResource::collection(Post::where('type',$type)->where('deleted_at',null)->get());
-            }
-            else{
-                return PostResource::collection(Post::where('type',$type)->where('author_id',$auth_user_id)->where('deleted_at',null)->get());
-            }
-
-        }
-        elseif($type == 3){
-            if(Auth()->user()->type == 1 ){
-                return PostResource::collection(Post::where('type',$type)->where('deleted_at',null)->get());
-            }
-            else{
-                return PostResource::collection(Post::where('type',$type)->where('author_id',$auth_user_id)->where('deleted_at',null)->get());
+        if ($type == 1) {
+            if (Auth()->user()->type == 1 || Auth()->user()->type == 9) {
+                return PostResource::collection(Post::where('type', $type)->where('deleted_at', null)->get());
+            } else {
+                return PostResource::collection(Post::where('type', $type)->where('author_id', $auth_user_id)->where('deleted_at', null)->get());
             }
 
-        }else{
-            return PostResource::collection(Post::where('type',$type)->where('deleted_at',null)->get());
+        } elseif ($type == 3) {
+            if (Auth()->user()->type == 1) {
+                return PostResource::collection(Post::where('type', $type)->where('deleted_at', null)->get());
+            } else {
+                return PostResource::collection(Post::where('type', $type)->where('author_id', $auth_user_id)->where('deleted_at', null)->get());
+            }
+
+        } else {
+            return PostResource::collection(Post::where('type', $type)->where('deleted_at', null)->get());
         }
 
     }
@@ -73,9 +70,10 @@ class PostController extends Controller
      * Show the form for creating a new resource.
      * @return Response
      */
-    public function detail($id){
+    public function detail($id)
+    {
         $post = Post::findOrfail($id);
-        if($post){
+        if ($post) {
             return new PostDetail($post);
             // return $post::with('tags','topics','contents','areas','categories')->first();
         }
@@ -83,20 +81,20 @@ class PostController extends Controller
 
     public function create(Request $request)
     {
-        $auth_user_id = Auth()->user()->id ;
+        $auth_user_id = Auth()->user()->id;
         $post = new Post;
         $post->shoulder = $request->shoulder;
         $post->headline = $request->headline;
         $post->hanger = $request->hanger;
         $post->reporter_id = $request->reporter['id'];
-        $post->author_id = $auth_user_id ;
-        $post->content   = $request['content'];
-        $post->designation   = $request->designation;
-        $post->expired_at = date('Y-m-d',strtotime($request->expired_at));
+        $post->author_id = $auth_user_id;
+        $post->content = $request['content'];
+        $post->designation = $request->designation;
+        $post->expired_at = date('Y-m-d', strtotime($request->expired_at));
         $post->share_at = $request->share_at;
-        if($request->status==1){
+        if ($request->status == 1) {
             $post->published_at = Carbon::now()->toDateTimeString();
-        }else{
+        } else {
             $post->published_at = null;
         }
         $post->status = $request->status;
@@ -104,10 +102,10 @@ class PostController extends Controller
         $post->instant_article = $request->instant_article;
 
         // featured photo
-        $post->featured_image_id = $request->featured_image_id ;
-        $post->featured_video_id = $request->featured_video_id ;
+        $post->featured_image_id = $request->featured_image_id;
+        $post->featured_video_id = $request->featured_video_id;
 
-        $post->created_by = $auth_user_id ;
+        $post->created_by = $auth_user_id;
         $post->save();
         // tag
         // $post->tags()->attach($request->tag_ids);
@@ -123,52 +121,49 @@ class PostController extends Controller
         $post->contents()->attach($request->content_ids);
         $post->scrolls()->attach($request->selected_scrolls);
 
-        $slug = str_replace(' ','-',$request->headline);
+        $slug = str_replace(' ', '-', $request->headline);
         $slug = strtolower($slug);
-        if(Post::where('slug',$slug)->first())
-        {
-            $post->slug = strtolower($slug.'-'.str_random(5));
-        }
-        else{
+        if (Post::where('slug', $slug)->first()) {
+            $post->slug = strtolower($slug . '-' . str_random(5));
+        } else {
             $post->slug = $slug;
         }
 
-        if($request->not_edit_image == 11){
+        if ($request->not_edit_image == 11) {
             $exploded = explode(',', $request->image);
             $decoded = base64_decode($exploded[1]);
-            if(str_contains($exploded[0], 'pdf'))
+            if (str_contains($exploded[0], 'pdf'))
                 $extension = 'pdf';
-            elseif(str_contains($exploded[0], 'jpeg'))
+            elseif (str_contains($exploded[0], 'jpeg'))
                 $extension = 'jpg';
             else
                 $extension = 'png';
 
-            $fileName = str_random().'.'.$extension;
-            $path = public_path().'/images/'.$fileName;
+            $fileName = str_random() . '.' . $extension;
+            $path = public_path() . '/images/' . $fileName;
             file_put_contents($path, $decoded);
-            $post->image = url('/').'/images/'.$fileName;
-        }
-        else{
+            $post->image = url('/') . '/images/' . $fileName;
+        } else {
             $post->image = $request->image;
         }
 
-        $post->job         = $request->job? json_encode($request->job):'';
-        $post->country     = $request->country? json_encode($request->country):'';
-        $post->language    = $request->language? json_encode($request->language):'';
-        $post->experience  = $request->experience;
+        $post->job = $request->job ? json_encode($request->job) : '';
+        $post->country = $request->country ? json_encode($request->country) : '';
+        $post->language = $request->language ? json_encode($request->language) : '';
+        $post->experience = $request->experience;
 
-        $post->job_id      = $request->job['id'];
-        $post->country_id  = $request->country['id'];
+        $post->job_id = $request->job['id'];
+        $post->country_id = $request->country['id'];
         $post->language_id = $request->language['id'];
 
         $post->experience_required = $request->experience_required;
-        $post->job_required        = $request->job_required;
-        $post->country_required    = $request->country_required;
-        $post->language_required   = $request->language_required;
+        $post->job_required = $request->job_required;
+        $post->country_required = $request->country_required;
+        $post->language_required = $request->language_required;
 
-        if($post->type == 1){
-            $applicant_id = SubscribeAgency::where('agency_id',$auth_user_id)->get()->pluck('applicant_id');
-            $users = User::whereIn('id',$applicant_id)->get();
+        if ($post->type == 1) {
+            $applicant_id = SubscribeAgency::where('agency_id', $auth_user_id)->get()->pluck('applicant_id');
+            $users = User::whereIn('id', $applicant_id)->get();
             foreach ($users as $key => $user) {
                 $data = [
                     'user' => $user,
@@ -180,28 +175,29 @@ class PostController extends Controller
 
         }
 
-        if($post->save()) {
+        if ($post->save()) {
             return new \Illuminate\Http\Resources\Json\JsonResource($post);
         }
         return 0;
     }
 
 
-    public function update(Request $request,$id){
+    public function update(Request $request, $id)
+    {
         $post = Post::findOrfail($id);
-        $auth_user_id = Auth()->user()->id ;
+        $auth_user_id = Auth()->user()->id;
         $post->shoulder = $request->shoulder;
         $post->headline = $request->headline;
         $post->hanger = $request->hanger;
         $post->reporter_id = $request->reporter['id'];
-        $post->author_id = $auth_user_id ;
-        $post->content   = $request['content'];
-        $post->designation   = $request->designation;
-        $post->expired_at = date('Y-m-d',strtotime($request->expired_at));
+        $post->author_id = $auth_user_id;
+        $post->content = $request['content'];
+        $post->designation = $request->designation;
+        $post->expired_at = date('Y-m-d', strtotime($request->expired_at));
         $post->share_at = $request->share_at;
-        if($request->status==1){
+        if ($request->status == 1) {
             $post->published_at = Carbon::now()->toDateTimeString();
-        }else{
+        } else {
             $post->published_at = null;
         }
         $post->status = $request->status;
@@ -209,10 +205,10 @@ class PostController extends Controller
         $post->instant_article = $request->instant_article;
 
         // featured photo
-        $post->featured_image_id = $request->featured_image_id ;
-        $post->featured_video_id = $request->featured_video_id ;
+        $post->featured_image_id = $request->featured_image_id;
+        $post->featured_video_id = $request->featured_video_id;
 
-        $post->created_by = $auth_user_id ;
+        $post->created_by = $auth_user_id;
         $post->save();
         // tag
         $post->tags()->attach($request->tag_ids);
@@ -228,38 +224,37 @@ class PostController extends Controller
         $post->contents()->attach($request->content_ids);
         $post->scrolls()->attach($request->selected_scrolls);
 
-        if($request->not_edit_image == 11){
+        if ($request->not_edit_image == 11) {
             $exploded = explode(',', $request->image);
             $decoded = base64_decode($exploded[1]);
-            if(str_contains($exploded[0], 'pdf'))
+            if (str_contains($exploded[0], 'pdf'))
                 $extension = 'pdf';
-            elseif(str_contains($exploded[0], 'jpeg'))
+            elseif (str_contains($exploded[0], 'jpeg'))
                 $extension = 'jpg';
             else
                 $extension = 'png';
 
-            $fileName = str_random().'.'.$extension;
-            $path = public_path().'/images/'.$fileName;
+            $fileName = str_random() . '.' . $extension;
+            $path = public_path() . '/images/' . $fileName;
             file_put_contents($path, $decoded);
-            $post->image = url('/').'/images/'.$fileName;
-        }
-        else{
+            $post->image = url('/') . '/images/' . $fileName;
+        } else {
             $post->image = $request->image;
         }
 
-        $post->job         = $request->job? json_encode($request->job):'';
-        $post->country     = $request->country? json_encode($request->country):'';
-        $post->language    = $request->language? json_encode($request->language):'';
-        $post->experience  = $request->experience;
+        $post->job = $request->job ? json_encode($request->job) : '';
+        $post->country = $request->country ? json_encode($request->country) : '';
+        $post->language = $request->language ? json_encode($request->language) : '';
+        $post->experience = $request->experience;
 
-        $post->job_id      = $request->job['id'];
-        $post->country_id  = $request->country['id'];
+        $post->job_id = $request->job['id'];
+        $post->country_id = $request->country['id'];
         $post->language_id = $request->language['id'];
 
         $post->experience_required = $request->experience_required;
-        $post->job_required        = $request->job_required;
-        $post->country_required    = $request->country_required;
-        $post->language_required   = $request->language_required;
+        $post->job_required = $request->job_required;
+        $post->country_required = $request->country_required;
+        $post->language_required = $request->language_required;
 
         if ($post->save()) {
             return new PostResource($post);
@@ -268,7 +263,8 @@ class PostController extends Controller
     }
 
 
-    public function reporterNews(Request $request){
+    public function reporterNews(Request $request)
+    {
 
         $posts = Post::all();
         return ReporterPost::collection($posts);
@@ -279,12 +275,13 @@ class PostController extends Controller
     {
         $post = Post::findOrfail($id);
         $post->deleted_at = now();
-        if($post->save()){
+        if ($post->save()) {
             return new PostResource($post);
         }
     }
 
-    public function updateStatus($id,$status){
+    public function updateStatus($id, $status)
+    {
         $post = Post::findOrfail($id);
         $post->status = $status;
         $post->published_at = Carbon::now()->toDateTimeString();
@@ -294,39 +291,47 @@ class PostController extends Controller
 
     public function search(Request $request)
     {
-        $post = Post::where('type',1);
-        if (isset($request->title))
-        {
-            $post = $post->Where('headline','LIKe',"%$request->title%");
+        $post = Post::where('type', 1)->where('salary','>=',100);
+//        dd(($post->get())->toArray());
+        if (isset($request->title) && !empty($request->title)) {
+            $post = $post->Where('headline', 'LIKe', "%$request->title%");
         }
-        if (isset($request->location))
-        {
-            $post = $post->Where('country_id',$request->location);
+        if (isset($request->location) && !empty($request->location)) {
+            $post = $post->Where('country_id', $request->location);
         }
-        if (isset($request->jobType))
-        {
+        if (isset($request->jobType) && !empty($request->jobType)) {
             $job = $request->jobType;
-            $post = $post->with('jobType')->WhereHas('jobType',function ($app) use($job){
-               $app->whereIn('id',$job);
+            $post = $post->with('jobType')->WhereHas('jobType', function ($app) use ($job) {
+                $app->whereIn('id', $job);
             });
         }
 
-        if (isset($request->senior))
-        {
+        if (isset($request->senior) && !empty($request->senior)) {
             $kk = $request->senior;
-            $post = $post->with('senior')->WhereHas('senior',function ($app) use($kk){
-                $app->where('id',$kk);
+            $post = $post->with('senior')->WhereHas('senior', function ($app) use ($kk) {
+                $app->where('id', $kk);
             });
         }
-        if (isset($request->gender))
-        {
+        if (isset($request->gender) && !empty($request->gender)) {
             $gender = $request->gender;
-            $post = $post->with('gender')->WhereHas('gender',function ($app) use($gender){
-                $app->where('id',$gender);
+            $post = $post->with('gender')->WhereHas('gender', function ($app) use ($gender) {
+                $app->where('id', $gender);
             });
         }
-        $post= $post->get();
 
-       return PostResource::collection($post);
+
+        $minSalary = $request->minSalary?(int)$request->minSalary:'';
+        $maxSalary = $request->maxSalary?(int)$request->maxSalary:'';
+
+        if ((isset($maxSalary) && !empty($maxSalary)) && (isset($minSalary) && !empty($minSalary))) {
+            $post = $post->whereBetween('salary', [$minSalary, $maxSalary]);
+        }elseif (isset($minSalary) && !empty($minSalary)) {
+            $post = $post->where('salary', '>',$minSalary);
+        }elseif (isset($maxSalary) && !empty($maxSalary)) {
+            $post = $post->where('salary', '<',$maxSalary);
+        }
+        $post =  $post->toSql();
+
+        return PostResource::collection($post);
     }
 }
---
+++ amsapi/bootstrap/cache/front_end_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\FrontEnd\\Providers\\FrontEndServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\FrontEnd\\Providers\\FrontEndServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
